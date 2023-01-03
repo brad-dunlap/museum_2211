c99b4ed5aeaea22c431858b472a219c75440b74e
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-03 11:24:38 -0500

    feat: tests for ability to add exhibits to Museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,6 +4,9 @@ require './lib/patron'
 
 RSpec.describe Museum do
 	let(:dmns){ Museum.new("Denver Museum of Nature and Science") }
+  let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+	let(:dead_sea_scrolls) { Exhibit.new({name: "Dead Sea Scrolls", cost: 10}) }
+	let(:imax) { Exhibit.new({name: "IMAX",cost: 15}) }
 
 	describe '#initialize' do
 		it 'exists' do
@@ -13,6 +16,15 @@ RSpec.describe Museum do
 		it 'has attributes' do
 			expect(dmns.name).to eq("Denver Museum of Nature and Science")
 			expect(dmns.exhibits).to eq([])
+		end
+	end
+
+	describe '#add_exhibit' do
+		it 'can add exhibits' do
+			dmns.add_exhibit(gems_and_minerals)
+			dmns.add_exhibit(dead_sea_scrolls)
+			dmns.add_exhibit(imax)
+			expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
 		end
 	end
 end
