20cb2458cfc5fe569e7ed8f1c2a7b91e6ffd357f
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-03 11:29:00 -0500

    feat: Can test for ability to recommend exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Museum do
   let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0}) }
 	let(:dead_sea_scrolls) { Exhibit.new({name: "Dead Sea Scrolls", cost: 10}) }
 	let(:imax) { Exhibit.new({name: "IMAX",cost: 15}) }
-
+	let(patron_1) { Patron.new("Bob", 20) }
+	let(patron_2) { patron_2 = Patron.new("Sally", 20) }
 	describe '#initialize' do
 		it 'exists' do
 			expect(dmns).to be_instance_of(Museum)
@@ -25,6 +26,16 @@ RSpec.describe Museum do
 			dmns.add_exhibit(dead_sea_scrolls)
 			dmns.add_exhibit(imax)
 			expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+		end
+	end
+
+	describe '#recommend_exhibits' do
+		it 'can recommend exhibits' do
+			patron_1.add_interest("Dead Sea Scrolls")
+			patron_1.add_interest("Gems and Minerals")
+			patron_2.add_interest("IMAX")
+			expect(dmns.recommend_exhibits(patron_1)).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+			expect(dmns.recommend_exhibits(patron_2)).to eq(["IMAX"])
 		end
 	end
 end
